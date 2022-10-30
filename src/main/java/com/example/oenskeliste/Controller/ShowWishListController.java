59a905128eb415f86675e8c109e5c99e3d091b29
Jakob <jako7096@stud.kea.dk>
2022-10-30 17:57:17 +0100

    Changes

---
+++ src/main/java/com/example/oenskeliste/Controller/ShowWishListController.java
@@ -0,0 +1,48 @@
+package com.example.oenskeliste.Controller;
+
+import com.example.oenskeliste.Service.WishlistService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
+
+import java.util.ArrayList;
+
+
+@RestController
+public class ShowWishListController {
+
+    WishlistService wls = new WishlistService();
+
+    @PostMapping("/getList")
+    public String getList(WebRequest req){
+
+        ArrayList<String> wishes = wls.getAllByPassword(req);
+        String name = wls.getName(req);
+
+        String list = "";
+
+        for (int i = 0; i < wishes.size(); i++) {
+            list = list + "\n" + wishes.get(i);
+        }
+
+        return "<!DOCTYPE html>\n" +
+                "<html lang=\"en\">\n" +
+                "<head>\n" +
+                "    <meta charset=\"UTF-8\">\n" +
+                "    <title>ReservedList</title>\n" +
+                "    <link rel=\"stylesheet\" href=\"index.css\">\n" +
+                "</head>\n" +
+                "<body>\n" +
+                "<h1 class=\"verdanaText\"> " + name + "'s ønskeliste </h1>\n" +
+                "\n" +
+                "<h2 class=\"verdanaText\">\n" +
+
+                list +
+                "\n" +
+                "    \n" +
+                "\n" +
+                "</h2>\n" +
+                "</body>\n" +
+                "</html>";
+    }
+}
